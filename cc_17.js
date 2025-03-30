9b57aeba8595bea98d0d268be997e4861abd2013
Dylan <safirodylan@gmail.com>
2025-03-30 19:31:50 -0400

    Task 2 = Created SalesRep class

--- cc_17.js
+++ cc_17.js
@@ -13,8 +13,32 @@ class Customer {
     };
 }; // Creates customer class with attributes
 
+// Task 2: Create a SalesRep Class
+class SalesRep {
+    constructor (name) {
+        this.name = name; // Sales rep name
+        this.clients = []; // Array of customer objects
+    };
+    addClient(customer) {
+        this.clients.push(customer); // Adds customer to the list
+        customer.salesRep = this; // Assigns sales rep to customer
+    };
+    getClientTotal (name) {
+        const client = this.clients.find(client => client.name === name); // Finds client by name
+        if (client) {
+            console.log(`Assigned Sales Rep: ${client.salesRep.name}, Total Spent by ${client.name}: $${client.getTotalSpent()}`); // If client is found -> outputs customer name and total spent
+        } else {
+            console.log(`Client not found.`) // Else client isn't found -> outputs client not found
+        }
+    };
+}; // Creates sales rep class with attributes
+
+// Test Cases
 const customer1 = new Customer ("Timmy Turner", "timaeusturnip@email.com"); // Creates new customer object
 customer1.addPurchase(485); // Adds purchase amount
 customer1.addPurchase(500); // Adds purchase amount to existing history
 console.log(`Customer Name: ${customer1.name}, Email: ${customer1.email}, Total Spent: $${customer1.getTotalSpent()}`); // Console logs customer information
 
+const salesRep1 = new SalesRep ("Ash Ketchum");
+salesRep1.addClient(customer1);
+salesRep1.getClientTotal("Timmy Turner");
