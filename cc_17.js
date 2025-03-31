b8963d59d8500f4209e285ac8da2b981a9ba606f
Dylan <safirodylan@gmail.com>
2025-03-30 21:10:49 -0400

    Task 3 – Extended VIPCustomer class

--- cc_17.js
+++ cc_17.js
@@ -28,10 +28,22 @@ class SalesRep {
         if (client) {
             console.log(`Assigned Sales Rep: ${client.salesRep.name}, Total Spent by ${client.name}: $${client.getTotalSpent()}`); // If client is found -> outputs customer name and total spent
         } else {
-            console.log(`Client not found.`) // Else client isn't found -> outputs client not found
-        }
+            console.log(`Client not found.`); // Else client isn't found -> outputs client not found
+        };
     };
 }; // Creates sales rep class with attributes
+
+// Task 3: Create a VIPCustomer Class (extends Customer)
+class VIPCustomer extends Customer {
+    constructor (name, email, vipLevel) { 
+        super(name, email); // Grabs customer name and email from customer class
+        this.vipLevel = vipLevel; // Customer VIP level
+    };
+    getTotalSpent() {
+        const loyaltyBonus = super.getTotalSpent() * 1.1; // Adds 10% loyalty bonus to total spent
+        return loyaltyBonus // Returns loyalty bonus
+    };
+}; // Creates VIP class with extended attributes
 
 // Test Cases
 const customer1 = new Customer ("Timmy Turner", "timaeusturnip@email.com"); // Creates new customer object
@@ -42,3 +54,7 @@ console.log(`Customer Name: ${customer1.name}, Email: ${customer1.email}, Total 
 const salesRep1 = new SalesRep ("Ash Ketchum");
 salesRep1.addClient(customer1);
 salesRep1.getClientTotal("Timmy Turner");
+
+const vipCust1 = new VIPCustomer ("Jimmy John", "jimothyjohnathon@email.com", "Gold")
+vipCust1.addPurchase(950)
+console.log(`VIP Customer Name: ${vipCust1.name}, Email: ${vipCust1.email}, Total Spent with Bonus: $${vipCust1.getTotalSpent()}`)
